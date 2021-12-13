112fc21781bb19728c438e43b9a0fe8ca070d3c7
Philip Eschenbacher <philip.eschenbacher@mongodb.com>
2021-12-13 08:16:33 +0100

    reverted to old group stage to keep both methods

--- realm_backend/MyBankGridView/functions/getGroupStage.js
+++ realm_backend/MyBankGridView/functions/getGroupStage.js
@@ -1,34 +1,74 @@
-exports = ({valueCols, groupToUse}) => {
+exports = ({rowGroupCols, groupKeys, valueCols}) => {
   const split = require("lodash/split");
   const last = require("lodash/last");
-  const upperFirst = require("lodash/upperFirst");
   
+  // find out about the lowest level of grouping and take this to create
+  // group stage in aggregation pipeline
+  const groupToUse = rowGroupCols.slice(groupKeys.length, groupKeys.length + 1);
   let project = {};
   let groupId = {};
-  let groupBody = {};
-  let pipeline = [];
+  let groupBody = {}; 
   
   if (groupToUse[0].id === "customerId") {
-    pipeline.push({"$unset": "accounts"});
-    pipeline.push({"$set": {"accounts.balance": "$totalBalance"}});
+    // Use special grouping on Customer-Level to have multiple fields attached
+    project = {
+      customerId: "$_id.customerId",
+      lastName: "$_id.lastName",
+      firstName: "$_id.firstName",
+      age: "$_id.age",
+      crmInformation: {
+        segmentation: "$_id.segmentation"
+      },
+      address: {
+        country: "$_id.country"
+      },
+      accounts: {
+        balance: "$balance"
+      }
+    }
+    
+    groupId = {
+      customerId: "$customerId",
+      lastName: "$lastName",
+      firstName: "$firstName",
+      age: "$age",
+      country: "$address.country",
+      segmentation: "$crmInformation.segmentation"
+    }
+    
+    groupBody = {
+      "balance": {
+        "$sum": "$accounts.balance"
+      }
+    }
   } else {
     project = convertDotPathToNestedObject(groupToUse[0].id, `$_id.${last(split(groupToUse[0].id, '.'))}`);
+  
+    // create all valueColums to calculate by the aggFunc set in Grid
+    // see GridOptions.js in client code
     groupBody = {};
     valueCols.forEach(element => {
-      groupBody = Object.assign(
-        groupBody,
-        {
-          [`total${upperFirst(element.field)}`]: {[`$${element.aggFunc}`]: `$total${upperFirst(element.field)}`}
-        });
-      project = Object.assign(project, convertDotPathToNestedObject(element.id, `$total${upperFirst(element.field)}`));
+      if (groupToUse[0].id === "customerId" || element.aggFunc !== "first") {
+        // if we have expectation for nested return, we need to nest them again because
+        // group will return un-nested values
+        project = Object.assign(project, convertDotPathToNestedObject(element.id, `$${last(split(element.id, '.'))}`));
+        groupBody = Object.assign(
+          groupBody,
+          {
+            [element.field]: {[`$${element.aggFunc}`]: `$${element.id}`}
+          });
+      }
     });
     
     // set group by objects
     groupId = {[last(split(groupToUse[0].id, '.'))]: `$${groupToUse[0].id}`};
-    pipeline.push({"$group": Object.assign({"_id": groupId}, groupBody)});
-    pipeline.push({"$set": project});
   }
 
+  const pipeline = [
+    {"$group": Object.assign({"_id": groupId}, groupBody)},
+    {"$set": project}
+  ];
+  
   return pipeline;
 };
 
@@ -44,15 +84,41 @@ function convertDotPathToNestedObject(path, value) {
 Testdata
 ========
 
-const groupToUse = [
+const rowGroupCols= [
     {
-        "id": "address.country",
+        "id": "age",
+        "displayName": "Age",
+        "field": "age"
+    },
+    {
+        "id": "country",
         "displayName": "Country",
         "field": "country"
+    },
+    {
+        "id": "_id",
+        "displayName": "Customer",
+        "field": "customer"
     }
 ]
 
+const groupKeys = [
+  31, "New Zealand"
+]
+
 const valueCols = [
+    {
+        "id": "lastName",
+        "aggFunc": "first",
+        "displayName": "Last Name",
+        "field": "lastName"
+    },
+    {
+        "id": "firstName",
+        "aggFunc": "first",
+        "displayName": "First Name",
+        "field": "firstName"
+    },
     {
         "id": "accounts.balance",
         "aggFunc": "sum",
@@ -61,6 +127,6 @@ const valueCols = [
     }
 ]
 
-exports({valueCols, groupToUse})
+exports({rowGroupCols, groupKeys, valueCols})
 
 */
