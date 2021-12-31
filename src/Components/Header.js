2b63a52172f08101926e129710529d15939aadf3
Philip Eschenbacher <philip.eschenbacher@mongodb.com>
2021-12-31 09:54:27 +0100

    added logos

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,7 @@
 import { RealmLogoLockup, AtlasLogoLockup, RealmLogoMark } from '@leafygreen-ui/logo';
 import * as React from 'react';
+import agGridLogo from '../Assets/ag-grid-logo.png';
+import reactLogo from '../Assets/react-logo.png';
 
 const Header = () => {
     return (
@@ -7,6 +9,10 @@ const Header = () => {
             <AtlasLogoLockup />
             <span style={{marginRight: 20}} />
             <RealmLogoLockup />
+            <span style={{marginRight: 20}} />
+            <img src={agGridLogo} style={{height: "20px", paddingBottom: 8}} />
+            <span style={{marginRight: 20}} />
+            <img src={reactLogo} height="35px" />
         </div>
         
     );
